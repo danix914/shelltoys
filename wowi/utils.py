37935fbc85aa4ed62bf280d8960f24fcbff6bb53
Det!C <danix914@gmail.com>
2022-01-22 17:06:51 +0800

    run shell command via subprocess

--- wowi/utils.py
+++ wowi/utils.py
@@ -1,3 +1,4 @@
+import subprocess
 import sys
 import time
 
@@ -18,3 +19,36 @@ def countdown(seconds, prefix_message='countdown'):
         time.sleep(1)
         count += 1
     print()
+
+
+def run_shell_cmd(cmd,
+                  *,
+                  show_cmd_newline=True,
+                  cmd_info=True,
+                  show_result=True):
+    """Execute linux commands via subprocess.Popen
+
+    Run commands via Popen, then return Popen object, STDOUT and STDERR.
+
+        Usage example:
+
+        proc, _, __ = run_shell_cmd('/tmp/myScript.sh')
+        run_shell_cmd('yum -y install tree')
+    """
+    if show_cmd_newline:
+        human_cmd = cmd.replace('\n', '\\n ')  # add a space for readability
+    else:
+        human_cmd = cmd
+    if cmd_info:
+        print('running command: `{human_cmd}`\n'
+              '  please wait a minute...')
+    p = subprocess.Popen(cmd, shell=True,
+                         stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p.communicate()
+    out = out.decode('utf8', 'backslashreplace')
+    err = err.decode('utf8', 'backslashreplace')
+
+    if show_result:
+        print('\n=== result ===\nSTDOUT:\n{}\nSTDERR:\n{}'.format(out, err))
+        print('=== Exit code: {} (command: `{}`) ===\n'.format(p.returncode, human_cmd))
+    return (p, out, err)
