6a43942dae2031ecd73dfd2b1f8541a596ce6c52
Det!C <danix914@gmail.com>
2022-01-24 16:07:30 +0800

    import functions at init

--- wowi/__init__.py
+++ wowi/__init__.py
@@ -2,6 +2,19 @@ import sys
 import logging
 from logging import NullHandler
 
+from wowi.utils import (
+    run_shell_cmd,
+    countdown,
+    resolve_path,
+    live_as_shell_cmd
+)
+
+__all__ = [
+    'countdown',
+    'run_shell_cmd',
+    'live_as_shell_cmd',
+    'resolve_path',
+]
 
 if sys.version_info < (3, 5):
     raise BaseException('Only support Python 3.5 or newer.')
